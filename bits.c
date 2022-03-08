0f9ec0d1790a5feac17aca6e79c180d39f373ea0
YushenHu0326 <62900433+YushenHu0326@users.noreply.github.com>
2022-03-08 12:53:22 -0500

    Update bits.c

--- bits.c
+++ bits.c
@@ -1,5 +1,5 @@
 /* 
- * <Please put your name and userid here>
+ * <Yushen Hu yh3856>
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
