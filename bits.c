b5e51190b013a7f945e45f486549814af189296e
YushenHu0326 <62900433+YushenHu0326@users.noreply.github.com>
2022-03-08 18:57:46 -0500

    Update bits.c

--- bits.c
+++ bits.c
@@ -162,7 +162,7 @@ int tmin(void) {
  *   Rating: 2
  */
 int isTmax(int x) {
-  return ~((x + 1) ^ x) + !(x + 1);
+  return !((~x ^ (x + 1)) | !(~x));
 }
 /* 
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
